d1e4e99b71f25f47dab8732f21901f9db521924e
JamesSilva99 <87684643+JamesSilva99@users.noreply.github.com>
2021-10-31 13:41:57 -0300

    Atualizado pq estava salvando dois

--- A3-saoJudas/src/main/java/dao/UsuarioDao.java
+++ A3-saoJudas/src/main/java/dao/UsuarioDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import model.Usuario;
 
 public class UsuarioDao {
@@ -15,7 +16,6 @@ public class UsuarioDao {
     public void insert(Usuario usuario) throws SQLException {
        
         String sqlInsert = "insert into usuario(nomeCompleto,email,telefone,usuario,senha) values (?,?,?,?,?)";
-
             PreparedStatement stm = connection.prepareStatement(sqlInsert);
             stm.setString(1, usuario.getNomeCompleto());
             stm.setString(2, usuario.getEmail());
@@ -27,13 +27,44 @@ public class UsuarioDao {
     }
     public boolean exiteUsuarioeSenha(Usuario usuario) throws SQLException {
         String sql = "select * from usuario where Usuario = ? and senha = ?";
-        PreparedStatement stm = connection.prepareStatement(sql);
+            PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, usuario.getUsuario());
             stm.setString(2, usuario.getSenha());
-        stm.execute();
+            stm.execute();
         
         ResultSet resultset = stm.getResultSet();
         return resultset.next();
     }
+    public ArrayList<Usuario> selectAll() throws SQLException{
+        String sql = "select * from usuario";
+        PreparedStatement stm = connection.prepareStatement(sql);
+        
+            return Pesquisa(stm);
+    }
 
+    private ArrayList<Usuario> Pesquisa(PreparedStatement stm) throws SQLException {
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+        
+        stm.execute();
+        ResultSet resultset = stm.getResultSet();
+        
+        while(resultset.next()){
+            String nomeCompleto = resultset.getString("nomeCompleto");
+            String email = resultset.getString("email");
+            String telefone = resultset.getString("telefone");
+            String usuario = resultset.getString("usuario");
+            String senha = resultset.getString("senha");
+            
+            Usuario todosUsuarios = new Usuario(nomeCompleto, email, telefone, usuario, senha);
+            usuarios.add(todosUsuarios);
+        }
+        return usuarios;
+    }
+    public Usuario porUsuario(Usuario usuario) throws SQLException{
+        String sql = "select * from usuario where usuario = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, usuario.getUsuario());
+            
+            return Pesquisa(stm).get(0);
+    }
 }
--- A3-saoJudas/src/main/java/view/TelaPrincipal.java
+++ A3-saoJudas/src/main/java/view/TelaPrincipal.java
@@ -5,6 +5,14 @@
  */
 package view;
 
+import dao.ConnectionFactory;
+import dao.UsuarioDao;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Usuario;
+
 /**
  *
  * @author James
@@ -27,45 +35,59 @@ public class TelaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
+        jMenuBar2 = new javax.swing.JMenuBar();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenu6 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
-        );
+        jMenu2.setText("Perfil");
 
-        jMenu1.setText("File");
-        jMenuBar1.add(jMenu1);
+        jMenuItem2.setText("Atualizar");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem2);
 
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
+        jMenuItem3.setText("Excluir");
+        jMenu2.add(jMenuItem3);
 
-        setJMenuBar(jMenuBar1);
+        jMenuBar2.add(jMenu2);
+
+        jMenu6.setText("Edit");
+        jMenuBar2.add(jMenu6);
+
+        setJMenuBar(jMenuBar2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        /*try {
+            Connection connection = new ConnectionFactory().obterConexao();
+            UsuarioDao usuariodao = new UsuarioDao(connection);
+            
+            Usuario usuarioselecionado = usuariodao.porUsuario();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TelaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -103,9 +125,10 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
 }
